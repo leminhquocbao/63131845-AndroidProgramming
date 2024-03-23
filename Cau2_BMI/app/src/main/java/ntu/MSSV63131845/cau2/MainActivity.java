987fbb9590405b6700c82d37207357a1abab7fb8
leminhquocbao <156556372+leminhquocbao@users.noreply.github.com>
2024-03-23 20:27:56 +0700

    câu 2 và câu 3

--- Cau2_BMI/app/src/main/java/ntu/MSSV63131845/cau2/MainActivity.java
+++ Cau2_BMI/app/src/main/java/ntu/MSSV63131845/cau2/MainActivity.java
@@ -13,44 +13,76 @@ public class MainActivity extends AppCompatActivity {
     EditText edtchieucao, edtcannang;
     TextView txtketqua;
     Button btntinhtoan;
+    RadioButton rdbtn1, rdbtn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edtchieucao = (EditText) findViewById(R.id.edtchieucao);
-        edtcannang = (EditText) findViewById(R.id.edtcannang);
-        txtketqua = (TextView) findViewById(R.id.txtketqua);
-        btntinhtoan = (Button) findViewById(R.id.btntinhtoan);
+        edtchieucao = findViewById(R.id.edtchieucao);
+        edtcannang = findViewById(R.id.edtcannang);
+        txtketqua = findViewById(R.id.txtketqua);
+        btntinhtoan = findViewById(R.id.btntinhtoan);
+        rdbtn1 = findViewById(R.id.rdbtn1);
+        rdbtn2 = findViewById(R.id.rdbtn2);
 
         btntinhtoan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                float chieucao = Float.parseFloat(edtchieucao.getText().toString()) / 100;
-                float cannang = Float.parseFloat(edtcannang.getText().toString());
+                float chieucao, cannang;
+
+                if (rdbtn2.isChecked()) {
+                    cannang = Float.parseFloat(edtcannang.getText().toString()) * 0.453592f;
+                    chieucao = Float.parseFloat(edtchieucao.getText().toString()) * 0.0254f;
+                } else {
+                    chieucao = Float.parseFloat(edtchieucao.getText().toString()) / 100;
+                    cannang = Float.parseFloat(edtcannang.getText().toString())*1000;
+                }
+
                 float bmi = cannang / (chieucao * chieucao);
-                float min=18.5F;
-                float max=25;
+                float min = 18.5f;
+                float max = 25f;
                 String ketqua;
+
+                // Xử lý kết quả BMI
                 if (bmi < 18.5) {
-                    float candoi=min-bmi;
-                    ketqua = "Bạn bị suy dinh dưỡng"+"\nBạn cần tăng thêm "+String.format("%.2f", candoi);
+                    float candoi = min - bmi;
+                    ketqua = "Bạn bị suy dinh dưỡng" + "\nBạn cần tăng thêm " + String.format("%.2f", candoi);
                 } else if (bmi < 25) {
                     ketqua = "Chỉ số BMI của bạn đang cân đối";
-                } else if ( bmi >=25 && bmi <=30) {
-                    float candoi =bmi-max;
-                    ketqua = "Bạn đang bị thừa cân"+"\nBạn cần giảm "+String.format("%.2f", candoi);
-                } else if (bmi>30 &&bmi<35){
-                    float candoi =bmi-max;
-                    ketqua = "Bạn đang bị thừa cân cấp II"+"\nBạn cần giảm "+String.format("%.2f", candoi);
+                } else if (bmi >= 25 && bmi <= 30) {
+                    float candoi = bmi - max;
+                    ketqua = "Bạn đang bị thừa cân" + "\nBạn cần giảm " + String.format("%.2f", candoi);
+                } else if (bmi > 30 && bmi < 35) {
+                    float candoi = bmi - max;
+                    ketqua = "Bạn đang bị thừa cân cấp II" + "\nBạn cần giảm " + String.format("%.2f", candoi);
                 } else {
-                    float candoi =bmi-max;
-                    ketqua = "Bạn đang bị thừa cân cấp III"+"\nBạn cần giảm "+String.format("%.2f", candoi);
+                    float candoi = bmi - max;
+                    ketqua = "Bạn đang bị thừa cân cấp III" + "\nBạn cần giảm " + String.format("%.2f", candoi);
                 }
 
                 txtketqua.setText("BMI: " + String.format("%.2f", bmi) + "\n" + ketqua);
             }
         });
+
+        rdbtn1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+
+                }
+            }
+        });
+
+        rdbtn2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+
+                }
+            }
+        });
+        rdbtn1.setChecked(true);
     }
 }
