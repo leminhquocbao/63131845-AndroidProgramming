286c06fc6fd4b4d2bab79aa63f6d8f9054a556ba
leminhquocbao <156556372+leminhquocbao@users.noreply.github.com>
2024-03-23 19:36:01 +0700

    câu 1

--- Cau1_BMI/src/ntu/MSSV63131845/TinhToan.java
+++ Cau1_BMI/src/ntu/MSSV63131845/TinhToan.java
@@ -49,7 +49,7 @@ public class TinhToan extends JFrame {
         unitComboBox = new JComboBox<>();
         unitComboBox.addItem("Kilogram & Meter");
         unitComboBox.addItem("Pound & Inch");
-        unitComboBox.addItem("Gram & Centimeter"); // Thêm tùy chọn mới
+        unitComboBox.addItem("Gram & Centimeter");
         unitComboBox.setBounds(17, 263, 180, 50);
         contentPane.add(unitComboBox);
 
@@ -92,20 +92,20 @@ public class TinhToan extends JFrame {
         double BMI;
 
         if (unitComboBox.getSelectedItem().equals("Pound & Inch")) {
-            cannang = convertPoundToKilogram(cannang);
-            chieucao = convertInchToMeter(chieucao);
+            cannang = Pound(cannang);
+            chieucao = Met(chieucao);
         } else if (unitComboBox.getSelectedItem().equals("Gram & Centimeter")) { 
-            cannang = convertGramToKilogram(cannang);
-            chieucao = convertCentimeterToMeter(chieucao);
+            cannang = gam(cannang);
+            chieucao = cm(chieucao);
         }
 
-        BMI = calculateBMI(cannang, chieucao);
+        BMI = BMI(cannang, chieucao);
 
         if (BMI < 18.5) {
             double candoi = mincandoi - BMI;
             txtThongBao.setText("Bạn đang bị suy dinh dưỡng: " + df.format(BMI) + ".\nBạn cần thêm " + df.format(candoi)
                     + " để được cân đối" + ".\nBạn cần ăn uống đầy đủ hơn để cân đối hơn."
-                    + ".\nLưu ý công thức và hiện chỉ số BMI đều được áp dụng bằng công thức của WHO");
+                    + "\nLưu ý công thức và hiện chỉ số BMI đều được áp dụng bằng công thức của WHO");
         } else if (BMI >= 18.5 && BMI < 25) {
             txtThongBao.setText("Thân hình của bạn đã rất cân đối, yêu cầu bạn tiếp tục duy trì: " + df.format(BMI));
         } else if (BMI >= 25 && BMI < 30) {
@@ -116,13 +116,13 @@ public class TinhToan extends JFrame {
         } else if (BMI >= 30 && BMI < 35) {
             double candoi = BMI - maxcandoi;
             txtThongBao.setText("Bạn đang thừa cân Cấp độ I: " + df.format(BMI) + ".\nBạn cần giảm "
-                    + df.format(candoi) + " để có thân hình cân đối."
+                    + df.format(candoi) + " để có thân hình cân đối"
                     + ".\nLưu ý công thức và hiện chỉ số BMI đều được áp dụng bằng công thức của WHO");
         } else if (BMI >= 35 && BMI < 40) {
             double candoi = BMI - maxcandoi;
             txtThongBao.setText("Bạn đang thừa cân Cấp độ II: " + df.format(BMI) + ".\nBạn cần giảm "
-                    + df.format(candoi) + "để có thân hình cân đối."
-                    + ".\nLưu ý công thức và hiện chỉ số BMI đều được áp dụng bằng công thức của WHO");
+                    + df.format(candoi) + " để có thân hình cân đối."
+                    + "\nLưu ý công thức và hiện chỉ số BMI đều được áp dụng bằng công thức của WHO");
         } else if (BMI >= 40) {
             double candoi = BMI - maxcandoi;
             txtThongBao.setText("Bạn đang thừa cân Cấp độ III: " + df.format(BMI) + ".\nBạn cần giảm "
@@ -131,37 +131,25 @@ public class TinhToan extends JFrame {
         }
     }
 
-    double calculateBMI(double weight, double height) {
-        return weight / (height * height);
+    double BMI(double chieucao, double cannang) {
+        return chieucao / (cannang * cannang);
     }
 
-    double convertPoundToKilogram(double weightInPound) {
-        return weightInPound * 0.453592;
+    double Pound(double cannang) {
+        return cannang * 0.453592;
     }
 
-    double convertInchToMeter(double heightInInch) {
-        return heightInInch * 0.0254;
+    double Met(double chieucao) {
+        return chieucao * 0.0254;
     }
 
-    double convertGramToKilogram(double weightInGram) {
-        return weightInGram / 1000.0;
+    double gam(double cannang) {
+        return cannang / 1000.0;
     }
 
-    double convertCentimeterToMeter(double heightInCentimeter) {
-        return heightInCentimeter / 100.0;
+    double cm(double chieucao) {
+        return chieucao / 100.0;
     }
 
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    TinhToan frame = new TinhToan();
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
 }
 
